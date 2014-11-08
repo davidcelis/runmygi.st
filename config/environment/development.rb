d831f55f9c3bf09ba59fe8e56e26a2d745166cd5
David Celis <me@davidcel.is>
2014-11-07 16:54:09 -0800

    Only configure Docker in development
    Signed-off-by: David Celis <me@davidcel.is>

--- config/environment/development.rb
+++ config/environment/development.rb
@@ -1,4 +1,14 @@
+require 'docker'
+
 # Settings specified here will take precedence over those in config/application.rb
 RunMyGist::Application.configure do
   config.log_level = :debug
+
+  cert_path = File.expand_path ENV['DOCKER_CERT_PATH']
+
+  Docker.options[:client_cert] = File.join(cert_path, 'cert.pem')
+  Docker.options[:client_key]  = File.join(cert_path, 'key.pem')
+  Docker.options[:ssl_ca_file] = File.join(cert_path, 'ca.pem')
+
+  Docker.options[:scheme] = 'https'
 end
